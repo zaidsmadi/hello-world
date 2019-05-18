a3850bd433eba7a05f1fb1d240115dd5eedd38c8
zaidsmadi <50780873+zaidsmadi@users.noreply.github.com>
2019-05-19 03:58:38 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,32 @@
-# hello-world
-test
+full_name = 'John Smith'
+age = 20
+is_new=True
+print(full_name, age, is_new)
+
+print('######################1')
+
+full_name = 'John Smith'
+age = str(20)
+is_new=True
+print(full_name, 'his age is ' + age, is_new)
+
+print('######################2')
+
+name = input('what is your name? ')
+favourate_color = input ('what is your favouate color? ')
+print (name + ' likes ' + favourate_color)
+birth_year = input('Birth year: ')
+age = 2019 - int(birth_year)
+print (age)
+
+print('######################3')
+
+user_weight_lbs=input('what is your weight? ')
+user_weight_lbs = float(user_weight_lbs )
+#weight_kg= float(user_weight_lbs)*0.454
+weight_kg= user_weight_lbs * 0.454
+user_weight_lbs = str(user_weight_lbs)
+weight_kg = str(weight_kg)
+print(f'Since your weight in LBs is {user_weight_lbs} lbs, then your equivalent weight in KGs is {weight_kg}')
+
+print('######################4')
